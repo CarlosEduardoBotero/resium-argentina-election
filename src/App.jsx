63a00c364865a317e04ae7595e8c61e33df1e5ed
Carlos Botero <boterocev@gmail.com>
2023-11-12 15:19:08 -0300

    init vite and installed dependecies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Viewer } from "resium";
+
+function App() {
+  return (
+    <>
+      <Viewer full></Viewer>
+    </>
+  );
+}
+
+export default App;
